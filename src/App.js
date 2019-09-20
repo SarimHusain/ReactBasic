a54d8cf905d3c21fe948c9825402ef4fa0c865b1
Alisamar Husain <zrthxn@gmail.com>
2019-09-20 22:56:36 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Box from "./Components/Box"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    text:"qwerty",
+    wext:"jgyhkl"
+  }
+
+  num = 9876
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.text}</p>
+        <p>{this.num}</p>
+
+        <Box/>
+
+        <button onClick={()=>{
+          this.num = 901
+          // this.setState({
+          //   text:"hgjh"
+          // })
+        }}>change</button>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/Box.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Box extends Component {
+  render() {
+    return (
+      <div style={{
+        margin: "1em",
+        padding: "1em",
+        border : "4px solid #000"
+      }}>
+        qwefrghjhgfdshjhgrfedwghjjhtgfredefhjjhgfd
+      </div>
+    )
+  }
+}
